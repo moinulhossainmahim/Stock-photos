b1e7c2fc34758c23f5ecc1d9c473f18a44e4d5b6
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-09-13 22:30:12 +0600

    Added all functionalities

--- src/App.js
+++ src/App.js
@@ -7,28 +7,97 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [photos, setPhotos]   = useState([])
+	const [loading, setLoading] = useState(false)
+	const [photos, setPhotos] = useState([])
+	const [page, setPage] = useState(0)
+	const [query, setQuery] = useState('')
 
-  const fetchImages = async () => {
-    setLoading(true)
-    let url
-    url = `${mainUrl}${clientID}`
-    try {   
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    } catch (error) {
-      setLoading(false)
-      console.log(error)
-    }
-  }
+	const fetchImages = async () => {
+		setLoading(true)
+		let url
+		const urlQuery = `&query=${query}`
+		const urlPage = `&page=${page}`
 
-  useEffect(() => {
-    fetchImages()
-  }, [])
+		if (query) {
+			url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+		} else {
+			url = `${mainUrl}${clientID}${urlPage}`
+		}
 
-  return <h2>stock photos starter</h2>
+		try {
+			const response = await fetch(url)
+			const data = await response.json()
+			setPhotos((prevPhotos) => {
+				if (query && page === 1) {
+					return data.results
+				} else if (query) {
+					return [...prevPhotos, ...data.results]
+				} else {
+					return [...prevPhotos, ...data]
+				}
+			})
+			setLoading(false)
+		} catch (error) {
+			setLoading(false)
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchImages()
+	}, [page])
+
+	useEffect(() => {
+		const event = window.addEventListener('scroll', () => {
+			if (
+				!loading &&
+				window.innerHeight + window.scrollY >=
+					document.body.scrollHeight - 2
+			) {
+				setPage((prevPage) => {
+					return prevPage + 1
+				})
+			}
+		})
+
+		return () => window.removeEventListener('scroll', event)
+	}, [])
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setPage(1)
+	}
+
+	return (
+		<main>
+			<section className='search'>
+				<form className='search-form'>
+					<input
+						type='text'
+						className='form-input'
+						placeholder='search'
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+					/>
+					<button
+						type='submit'
+						className='submit-btn'
+						onClick={handleSubmit}
+					>
+						<FaSearch />
+					</button>
+				</form>
+			</section>
+			<section className='photos'>
+				<div className='photos-center'>
+					{photos.map((photo) => {
+						return <Photo key={photo.id} photo={photo} />
+					})}
+				</div>
+				{loading && <h2 className='loading'>Loading...</h2>}
+			</section>
+		</main>
+	)
 }
 
 export default App
